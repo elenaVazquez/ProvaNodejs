620db922ef5a2f3d7050b0e57e6bfb0ea79ed33a
elenaVazquez <evazquezdaza@gmail.com>
2012-09-27 09:59:46 +0200

    prova

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 var http = require("http");
 
-
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("Primera prova!!!!!!!!!!!!");
