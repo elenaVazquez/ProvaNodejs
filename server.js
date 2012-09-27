d409329d7061cf50b03ae25f0b67ad63413d95d0
unknown <elena.vazquez@jiraya.safelayer.lan>
2012-09-27 09:49:59 +0200

    pujada del projecte

---
+++ server.js
@@ -0,0 +1,10 @@
+
+
+var http = require("http");
+
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Primera prova!!!!!!!!!!!!");
+  response.end();
+}).listen(8888);
